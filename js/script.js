cb5e4f9b242c853df3772ef0a22aecb94f784d04
Michael Costa <mcostab94@gmail.com>
2024-04-22 16:50:33 -0300

    correcao

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump =() => {
     mario.classList.add('jump');
 
     setTimeout(() => {
 
-        mario.classList.remove('jump');
+    mario.classList.remove('jump');
 
     }, 500);
 
 }
 
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    console.log(pipePosition)
+    
+}, 10);
+
 document.addEventListener('keydown', jump );
+
+
+
+
+
 
 document.getElementById('botaoEspaco').addEventListener('click', function() {
     var evento = new KeyboardEvent('keydown', {
