1b28fc25c7dd1702a4ba4a02ab4062db2fc34d06
Michael Costa <mcostab94@gmail.com>
2024-04-22 15:25:06 -0300

    att

--- js/script.js
+++ js/script.js
@@ -13,3 +13,11 @@ const jump =() => {
 
 document.addEventListener('keydown', jump );
 
+document.getElementById('botaoEspaco').addEventListener('click', function() {
+    var evento = new KeyboardEvent('keydown', {
+      key: ' ',
+      keyCode: 32,
+      which: 32
+    });
+    document.dispatchEvent(evento);
+  });
